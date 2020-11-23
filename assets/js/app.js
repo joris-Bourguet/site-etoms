78162b0230a092e6e5414ff28ac766b6d4e2e1d2
Sandwop <stanjacob@hotmail.fr>
2020-11-23 17:51:44 +0100

    btns
    j'ai juste changé la couleur des RS et mis un hover pour l'instant, je continuerai demain ;)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,8 @@
-let btnScroll = document.querySelector(".scrollBot")
-console.log(btnScroll);
+const btnScroll = document.querySelector(".scrollBot");
+const topDiv2 = document.querySelector("#test")
+
+btnScroll.addEventListener("click", () => {
+    window.scrollTo(0, topDiv2.offsetTop);
+})
+
+console.log(topDiv2.offsetTop)
