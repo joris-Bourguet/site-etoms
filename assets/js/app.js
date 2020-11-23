eaa37f95a706299dc18c7d24cdbc8a7919150b0c
Sandwop <stanjacob@hotmail.fr>
2020-11-23 20:59:32 +0100

    top btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,14 @@ const btnScroll = document.querySelector(".scrollBot");
 const btnScroll_ = document.querySelector(".scrollBtn");
 const topDiv2 = document.querySelector("#team")
 const topDiv3 = document.querySelector("#test")
+mybutton = document.getElementById("topBtn");
 
+// BOUTON RETOURNER AU TOP
+mybutton = document.getElementById("topBtn");
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
 
 // SCROLL VERS SECTION 2
 btnScroll.addEventListener("click", () => {
