77ad24d181b32ed6c1047a73d129c7d823e076eb
Sandwop <stanjacob@hotmail.fr>
2020-11-23 20:38:43 +0100

    chevron + scroll section 3
    J'ai rajouté le chevron pour scroll à la section 3, faudra qu'on revoit comment positionner le chevron parce que je l'ai fait à l'arrache mdr mais normalement le js et le html est bon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,16 @@
 const btnScroll = document.querySelector(".scrollBot");
+const btnScroll_ = document.querySelector(".scrollBtn");
 const topDiv2 = document.querySelector("#team")
 const topDiv3 = document.querySelector("#test")
 
+
+// SCROLL VERS SECTION 2
 btnScroll.addEventListener("click", () => {
     window.scrollTo(0, topDiv2.offsetTop);
+})
+
+
+// SCROLL VERS SECTION 3
+btnScroll_.addEventListener("click", () => {
+    window.scrollTo(0, topDiv3.offsetTop);
 })
