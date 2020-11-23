8aaf60d7ef34781443548c2b8ca1671c57aeaad7
Sandwop <stanjacob@hotmail.fr>
2020-11-23 18:29:32 +0100

    page 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
 const btnScroll = document.querySelector(".scrollBot");
-const topDiv2 = document.querySelector("#test")
+const topDiv2 = document.querySelector("#team")
+const topDiv3 = document.querySelector("#test")
 
 btnScroll.addEventListener("click", () => {
     window.scrollTo(0, topDiv2.offsetTop);
 })
-
-console.log(topDiv2.offsetTop)
