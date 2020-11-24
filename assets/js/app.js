955935c72a4ad507daa6279dd33269313c2be805
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2020-11-24 15:16:27 +0100

    avancement apres test merge

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,3 @@
-const btnScroll = document.querySelector(".scrollBot");
-const btnScroll_ = document.querySelector(".scrollBtn");
-const topDiv2 = document.querySelector("#team")
-const topDiv3 = document.querySelector("#test")
-mybutton = document.getElementById("topBtn");
+import { navigation } from "./navigations.js";
 
-// BOUTON RETOURNER AU TOP
-mybutton = document.getElementById("topBtn");
-
-function topFunction() {
-    document.documentElement.scrollTop = 0;
-}
-
-// SCROLL VERS SECTION 2
-btnScroll.addEventListener("click", () => {
-    window.scrollTo(0, topDiv2.offsetTop);
-})
-
-
-// SCROLL VERS SECTION 3
-btnScroll_.addEventListener("click", () => {
-    window.scrollTo(0, topDiv3.offsetTop);
-})
+navigation()
---
+++ assets/js/navigations.js
@@ -0,0 +1,29 @@
+function navigation() {
+    const btnScroll = document.querySelector(".scrollBot");
+    const btnScroll_ = document.querySelector(".scrollBtn");
+    const topDiv2 = document.querySelector("#team")
+    const topDiv3 = document.querySelector("#test")
+    const scrollTop = document.getElementById("topBtn")
+
+    // click = scroll to top
+    scrollTop.addEventListener('click', () => {
+        scrollToTop()
+    })
+
+    function scrollToTop() {
+        document.documentElement.scrollTop = 0;
+    }
+
+    // SCROLL VERS SECTION 2
+    btnScroll.addEventListener("click", () => {
+        window.scrollTo(0, topDiv2.offsetTop);
+    })
+
+
+    // SCROLL VERS SECTION 3
+    btnScroll_.addEventListener("click", () => {
+        window.scrollTo(0, topDiv3.offsetTop);
+    })
+}
+
+export { navigation }
