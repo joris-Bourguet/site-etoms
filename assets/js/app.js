2aa630952f2e3853bb26635ece7c05e3ab5154e9
joris-Bourguet <43138838+joris-Bourguet@users.noreply.github.com>
2020-12-03 16:39:51 +0100

    js-marque

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,22 @@ import { menu } from "./menu.js";
 import { header } from "./header.js";
 import { blog } from "./blog.js";
 import { boutique } from "./boutique.js";
-import { cartable } from "./cartable.js"
-import { marque } from "./marque.js"
+import { cartable } from "./cartable.js";
+import { marque } from "./marque.js";
 
 header()
 menu()
 
-if (document.URL.includes("index.html")) {
+if (document.URL.includes("index")) {
     navigation()
-} else if (document.URL.includes("contact.html")) {
+} else if (document.URL.includes("contact")) {
     contact()
-} else if (document.URL.includes("blog.html")) {
+} else if (document.URL.includes("blog")) {
     blog()
-} else if (document.URL.includes("boutique.html")) {
+} else if (document.URL.includes("boutique")) {
     boutique()
-} else if (document.URL.includes("cartable.html")) {
-    cartable()
-} else if (document.URL.includes("marque.html")) {
+} else if (document.URL.includes("marque")) {
     marque()
+} else if (document.URL.includes("cartable")) {
+    cartable()
 }
